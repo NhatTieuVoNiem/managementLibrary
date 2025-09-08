1470289d0254aac3070174efd8cef218f42c492d
Admin <Admin@DESKTOP-6TMLJEV>
2025-09-08 10:12:30 +0700

    cap nhat code

--- QuanLyThuVien/FormHome.Designer.cs
+++ QuanLyThuVien/FormHome.Designer.cs
@@ -98,9 +98,166 @@
             // 
             // quảnLýHóaĐơnToolStripMenuItem
             // 
-            this.quảnLýHóaĐơnToolStripMenuItem.Name = "quảnLýHóaĐơnToolStripMenuItem";
-            this.quảnLýHóaĐơnToolStripMenuItem.Size = new System.Drawing.Size(257, 30);
-            this.quảnLýHóaĐơnToolStripMenuItem.Text = "Quản lý hóa đơn";
+            this.dịGiớiToolStripMenuItem.Name = "dịGiớiToolStripMenuItem";
+            this.dịGiớiToolStripMenuItem.Size = new System.Drawing.Size(213, 30);
+            this.dịGiớiToolStripMenuItem.Text = "Dị giới";
+            // 
+            // quânSựToolStripMenuItem
+            // 
+            this.quânSựToolStripMenuItem.Name = "quânSựToolStripMenuItem";
+            this.quânSựToolStripMenuItem.Size = new System.Drawing.Size(213, 30);
+            this.quânSựToolStripMenuItem.Text = "Quân sự";
+            // 
+            // lịchSửToolStripMenuItem
+            // 
+            this.lịchSửToolStripMenuItem.Name = "lịchSửToolStripMenuItem";
+            this.lịchSửToolStripMenuItem.Size = new System.Drawing.Size(213, 30);
+            this.lịchSửToolStripMenuItem.Text = "Lịch sử";
+            // 
+            // xuyênKhôngToolStripMenuItem
+            // 
+            this.xuyênKhôngToolStripMenuItem.Name = "xuyênKhôngToolStripMenuItem";
+            this.xuyênKhôngToolStripMenuItem.Size = new System.Drawing.Size(213, 30);
+            this.xuyênKhôngToolStripMenuItem.Text = "Xuyên không";
+            // 
+            // trọngSinhToolStripMenuItem
+            // 
+            this.trọngSinhToolStripMenuItem.Name = "trọngSinhToolStripMenuItem";
+            this.trọngSinhToolStripMenuItem.Size = new System.Drawing.Size(213, 30);
+            this.trọngSinhToolStripMenuItem.Text = "Trọng sinh";
+            // 
+            // trinhThámToolStripMenuItem
+            // 
+            this.trinhThámToolStripMenuItem.Name = "trinhThámToolStripMenuItem";
+            this.trinhThámToolStripMenuItem.Size = new System.Drawing.Size(213, 30);
+            this.trinhThámToolStripMenuItem.Text = "Trinh thám";
+            // 
+            // thiếuNhiToolStripMenuItem
+            // 
+            this.thiếuNhiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.truyệnNgụNgônToolStripMenuItem,
+            this.truyệnCổTíchToolStripMenuItem});
+            this.thiếuNhiToolStripMenuItem.Name = "thiếuNhiToolStripMenuItem";
+            this.thiếuNhiToolStripMenuItem.Size = new System.Drawing.Size(198, 30);
+            this.thiếuNhiToolStripMenuItem.Text = "Thiếu nhi";
+            // 
+            // truyệnNgụNgônToolStripMenuItem
+            // 
+            this.truyệnNgụNgônToolStripMenuItem.Name = "truyệnNgụNgônToolStripMenuItem";
+            this.truyệnNgụNgônToolStripMenuItem.Size = new System.Drawing.Size(244, 30);
+            this.truyệnNgụNgônToolStripMenuItem.Text = "Truyện ngụ ngôn";
+            // 
+            // truyệnCổTíchToolStripMenuItem
+            // 
+            this.truyệnCổTíchToolStripMenuItem.Name = "truyệnCổTíchToolStripMenuItem";
+            this.truyệnCổTíchToolStripMenuItem.Size = new System.Drawing.Size(244, 30);
+            this.truyệnCổTíchToolStripMenuItem.Text = "Truyện cổ tích";
+            // 
+            // xếpHạngToolStripMenuItem
+            // 
+            this.xếpHạngToolStripMenuItem.Name = "xếpHạngToolStripMenuItem";
+            this.xếpHạngToolStripMenuItem.Size = new System.Drawing.Size(108, 29);
+            this.xếpHạngToolStripMenuItem.Text = "Xếp hạng";
+            // 
+            // bộLọcToolStripMenuItem
+            // 
+            this.bộLọcToolStripMenuItem.Name = "bộLọcToolStripMenuItem";
+            this.bộLọcToolStripMenuItem.Size = new System.Drawing.Size(84, 29);
+            this.bộLọcToolStripMenuItem.Text = "Bộ lọc";
+            // 
+            // bìnhLuậnToolStripMenuItem
+            // 
+            this.bìnhLuậnToolStripMenuItem.Name = "bìnhLuậnToolStripMenuItem";
+            this.bìnhLuậnToolStripMenuItem.Size = new System.Drawing.Size(109, 29);
+            this.bìnhLuậnToolStripMenuItem.Text = "Bình luận";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.35345F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.64655F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 497F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 172F));
+            this.tableLayoutPanel1.Controls.Add(this.btnTimKiem, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.linkLabel1, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 33);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 31.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 68.75F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 335F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1147, 534);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.BackColor = System.Drawing.Color.Violet;
+            this.btnTimKiem.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTimKiem.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.Location = new System.Drawing.Point(977, 3);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(167, 33);
+            this.btnTimKiem.TabIndex = 0;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(480, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(491, 33);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 2);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(119, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(852, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Truyện tiên hiệp";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.linkLabel1.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(977, 162);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(167, 36);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Xem thêm";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 3);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(119, 201);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1025, 330);
+            this.tableLayoutPanel2.TabIndex = 4;
             // 
             // FormHome
             // 
